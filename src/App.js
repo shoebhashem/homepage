b78ceebf84e5f7d7b042c16fb5e9a899af2ab018
Shoeb Hashem <shoeb.hashem@gmail.com>
2024-04-08 21:37:03 +0200

    first issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Homepage</h1>
+        <p>This is a basic example of a homepage created with React.js</p>
       </header>
     </div>
   );
